d091917dbf7a09e570dd59feb6d7a1ce184ab25e
xy-kia <2993405609@qq.com>
2026-04-03 20:34:49 +0800

    add test case: should_return_heading_s_given_command_is_r_and_facing_is_e

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -115,4 +115,18 @@ mod move_tests{
         assert_eq!(expected_pose, executor.query());
         //判断
     }
+
+    #[test]
+    fn should_return_heading_s_given_command_is_r_and_facing_is_e(){
+        //测试e方向下r指令
+        let original_pose = Pose::new(0,0,'E');
+        //初始位置
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("R");
+        //转向
+        let expected_pose = Pose::new(0,0,'S');
+        //预测位置
+        assert_eq!(expected_pose, executor.query());
+        //判断
+    }
 }
