700f6066aa3092ba1dabb576c88317cb4d3f9c56
xy-kia <2993405609@qq.com>
2026-04-03 20:00:05 +0800

    add test case: should_return_y_plus_1_given_command_is_m_and_facing_is_n

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -17,6 +17,7 @@ mod move_tests{
         assert_eq!(expected_pose, executor.query());
         //判断
     }
+
     #[test]
     fn should_return_x_minus_1_given_command_is_m_and_facing_is_w(){
         //测试w方向下m指令
@@ -26,6 +27,20 @@ mod move_tests{
         executor.execute("M");
         //移动
         let expected_pose = Pose::new(-1,0,'W');
+        //预测位置
+        assert_eq!(expected_pose, executor.query());
+        //判断
+    }
+
+    #[test]
+    fn should_return_y_plus_1_given_command_is_m_and_facing_is_n(){
+        //测试n方向下m指令
+        let original_pose = Pose::new(0,0,'N');
+        //初始位置
+        let mut executor = Executor::with_pose(original_pose);
+        executor.execute("M");
+        //移动
+        let expected_pose = Pose::new(0,1,'N');
         //预测位置
         assert_eq!(expected_pose, executor.query());
         //判断
